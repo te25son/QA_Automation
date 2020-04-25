b56d6e3645fbbdfd3b359f1dc680ff7c0f8fa920
te25son <te25son@gmail.com>
2020-04-25 11:08:30 +0200

    Add while loop to program

--- Palindrome/src/Palindrome/Program.cs
+++ Palindrome/src/Palindrome/Program.cs
@@ -6,17 +6,26 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a word, phrase, or sentence.");
-            var input = Console.ReadLine();
-            var palindromeChecker = new PalindromeChecker(input);
+            while (true)
+            {
+                Console.WriteLine("Enter a word, phrase, or sentence. Press 'q' to quit.");
+                var input = Console.ReadLine();
 
-            if (palindromeChecker.IsPalindrome)
-            {
-                Console.WriteLine($"{input} is a palindrome.");
-            }
-            else
-            {
-                Console.WriteLine($"{input} is not a palindrome.");
+                if (input.Equals("q"))
+                {
+                    break;
+                }
+
+                var palindromeChecker = new PalindromeChecker(input);
+
+                if (palindromeChecker.IsPalindrome)
+                {
+                    Console.WriteLine($"{input} is a palindrome.\n");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is not a palindrome.\n");
+                }
             }
         }
     }
