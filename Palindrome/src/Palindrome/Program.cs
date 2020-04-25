504897876d133e0cf31a39d3ffc83104ff2f7c4a
te25son <te25son@gmail.com>
2020-04-25 11:04:49 +0200

    Add logic to palindrome program

--- Palindrome/src/Palindrome/Program.cs
+++ Palindrome/src/Palindrome/Program.cs
@@ -9,6 +9,15 @@ namespace Palindrome
             Console.WriteLine("Enter a word, phrase, or sentence.");
             var input = Console.ReadLine();
             var palindromeChecker = new PalindromeChecker(input);
+
+            if (palindromeChecker.IsPalindrome)
+            {
+                Console.WriteLine($"{input} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is not a palindrome.");
+            }
         }
     }
 }
