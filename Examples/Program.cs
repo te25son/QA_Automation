ef7f3741912873a546b43eb69248e78811551194
te25son <te25son@gmail.com>
2020-05-05 07:52:33 +0200

    Add examples project

---
+++ Examples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace examples
+{
+    public delegate void EnrollStudent(Student student);
+    class Program
+    {
+        static void Enroll(out Student student)
+        {
+            student = new Student();
+            student.Enrolled = false;
+        }
+
+        static void Main(string[] args)
+        {
+            Student student;
+            Enroll(out student);
+
+            Console.WriteLine(student.Enrolled);
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public bool Enrolled { get; set; }
+    }
+}
