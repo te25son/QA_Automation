8c0e7a0486ae89f3d7661b4a15c2e238ef32b182
te25son <te25son@gmail.com>
2020-07-22 09:10:40 +0200

    Add lesson one

---
+++ Lessons/Lessons/Lesson01/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Lessons.Lesson01
+{
+    public class Book
+    {
+        public List<double> grades { get; private set; }
+
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public double GetAverageGrade()
+        {
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            return result /= grades.Count;
+        }
+    }
+}
